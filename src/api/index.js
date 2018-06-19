073c263acee31205465ee96b191d2611597edf34
stvmachine <campos.esteban@gmail.com>
2018-06-19 22:40:50 +1000

    added api

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const URL_SERVER =
+  "https://l94wc2001h.execute-api.ap-southeast-2.amazonaws.com/prod/fake-auth";
+
+export function postRequest(input) {
+  const { name, email } = input;
+
+  return axios
+    .post(URL_SERVER, {
+      name,
+      email
+    })
+    .then(response => {
+      if (response && response.status === 200 && response.data) {
+        return response.data;
+      } else {
+        return Promise.reject({ error: "Unexpected error" });
+      }
+    })
+    .catch(error => {
+      const response = error.response;
+      return response && response.data && response.data.errorMessage
+        ? response.data.errorMessage
+        : "Unexpected error";
+    });
+}
